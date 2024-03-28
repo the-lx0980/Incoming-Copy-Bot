61e36251fd5322f564e119f48cf47164b0847a0f
Protullen <138602811+protullen@users.noreply.github.com>
2024-03-28 21:19:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     API_ID = int(getenv("API_ID", 0))
     API_HASH = getenv("API_HASH")
     SESSION = getenv("SESSION")
-    CHANNEL_ID = -1001912424642
+    CHANNEL_ID = int(getenv("CHANNEL_ID"))
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
