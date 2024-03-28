e90fb522bac8735c4e363df506288f4a173aa9e7
Protullen <138602811+protullen@users.noreply.github.com>
2024-03-28 21:37:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
-from os import getenv
+from os import getenv, environ
+import re
 import logging
+
+id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     API_ID = int(getenv("API_ID", 0))
     API_HASH = getenv("API_HASH")
     SESSION = getenv("SESSION")
-    CHANNEL_ID = int(getenv("CHANNEL_ID"))
+    FROM_CHAT = [int(ch) if id_pattern.search(ch) else ch for ch in environ['FROM_CHAT'].split()]
+    TO_CHAT = int(getenv("TO_CHAT"))
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
