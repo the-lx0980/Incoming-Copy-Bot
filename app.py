df9006557febd15442ccbf7a98cf8514c3b9dd86
Protullen <138602811+protullen@users.noreply.github.com>
2024-03-28 22:09:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'User-Forward-Copy-Bot'
+    return 'movies 1782 User Bot'
 if __name__ == "__main__":
     app.run()
