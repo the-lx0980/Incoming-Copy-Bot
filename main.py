95720c6fd2f57a072a4646263a7691e0cf796914
protullen <138602811+protullen@users.noreply.github.com>
2024-06-19 23:28:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,21 @@ Userbot = Client(
 async def start(bot, update):
     await update.reply("Zinda hain.... 😐")
 
-@Userbot.on_message(media_filter)
+@Userbot.on_message(filters.channel & media_filter)
 async def forward(bot, update):
+    try:
+        await bot.copy_message(
+            chat_id=Config.CHANNEL_ID,
+            from_chat_id=update.chat.id,
+            message_id=update.id,
+            caption=f"**{update.caption}**",
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+    except Exception as e:
+        print(e) 
+
+@Userbot.on_message(filters.group & media_filter)
+async def forward_group(bot, update):
     try:
         await bot.copy_message(
             chat_id=Config.CHANNEL_ID,
