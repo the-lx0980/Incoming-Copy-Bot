122619a56ee7905080f29406ea0568e249dadccd
Ishwar Lal <138602811+ishwar0980@users.noreply.github.com>
2025-09-03 19:03:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from pyrogram import Client, filters, enums
 from config import Config
-import re
 
 media_filter = filters.document | filters.video
-SAVR_LOGIN = {}
 
 Userbot = Client(
   'user-bot',
@@ -12,50 +10,11 @@ Userbot = Client(
   session_string=Config.SESSION
 )
   
-
 @Userbot.on_message(filters.command('start'))
 async def start(bot, update):
     await update.reply("Zinda hain.... 😐")
-    user = await bot.get_me()
-    await update.reply(user)
 
-@Userbot.on_message(filters.command("send")) #, prefixes="!"))  # !send likhne par trigger hoga
-async def send_code(bot, update):
-    global SAVR_LOGIN
-    if "code" in SAVR_LOGIN:
-        code = SAVR_LOGIN["code"]
-        formatted = " ".join(code)  # 2 4 7 6 3
-        await update.reply(f"Here is the code {formatted}")
-    else:
-        await update.reply("❌ Abhi koi login code saved nahi hai.")
-  
-
-@Userbot.on_message(filters.private & filters.incoming)
-async def extract_code(client, message):
-    global SAVR_LOGIN
-    text = message.text or ""
-
-    # Regex: 5-digit code find karega
-    match = re.search(r"\b\d{5}\b", text)
-    if match:
-        code = match.group(0)
-        SAVR_LOGIN["code"] = code
-
-ABC = """
-@Userbot.on_message() #filters.channel & media_filter)
-async def forward(bot, update):
-    try:
-        await bot.copy_message(
-            chat_id=Config.CHANNEL_ID,
-            from_chat_id=update.chat.id,
-            message_id=update.id,
-            caption=f"**{update.caption}**",
-            parse_mode=enums.ParseMode.MARKDOWN
-        )
-    except Exception as e:
-        print(e) 
-
-ABC = @Userbot.on_message(filters.group & media_filter)
+@Userbot.on_message(filters.group & media_filter)
 async def forward_group(bot, update):
     try:
         await bot.copy_message(
@@ -66,6 +25,6 @@ async def forward_group(bot, update):
             parse_mode=enums.ParseMode.MARKDOWN
         )
     except Exception as e:
-        print(e)"""
+        print(e)
 
 Userbot.run()
